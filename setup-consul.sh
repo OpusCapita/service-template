ce2e993af754ce6b817a7c1a3deb0aa8ac7bc929
Christian Drengenberg <kwierchris@users.noreply.github.com>
2018-03-02 15:23:05 +0100

    Update setup-consul.sh

--- setup-consul.sh
+++ setup-consul.sh
@@ -2,10 +2,10 @@ function putConsulData
 {
     curl -X PUT -d ${MYSQL_DATABASE} http://consul:8500/v1/kv/{{your-service-name}}/db-init/database &&
     curl -X PUT -d 'root' http://consul:8500/v1/kv/{{your-service-name}}/db-init/user &&
-    curl -X PUT -d ${MYSQL_ROOT_PASSWORD} http://consul:8500/v1/kv/{{your-service-name}}/db-init/password %%
-    curl -X PUT -d 'true' http://consul:8500/v1/kv/{{your-service-name}}/db-init/populate-test-data %%
-    curl -X PUT -d ${REDIS_AUTH} http://consul:8500/v1/kv/{{your-service-name}}/redis/password %%
-    curl -X PUT -d ${RABBITMQ_USER} http://consul:8500/v1/kv/{{your-service-name}}/mq/user %%
+    curl -X PUT -d ${MYSQL_ROOT_PASSWORD} http://consul:8500/v1/kv/{{your-service-name}}/db-init/password &&
+    curl -X PUT -d 'true' http://consul:8500/v1/kv/{{your-service-name}}/db-init/populate-test-data &&
+    curl -X PUT -d ${REDIS_AUTH} http://consul:8500/v1/kv/{{your-service-name}}/redis/password v
+    curl -X PUT -d ${RABBITMQ_USER} http://consul:8500/v1/kv/{{your-service-name}}/mq/user &&
     curl -X PUT -d ${RABBITMQ_PASS} http://consul:8500/v1/kv/{{your-service-name}}/mq/password
 }
 
